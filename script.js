a53409ceec2a1e172f659fd9e23b5f34814024bb
aligreen <aligreen@ccs.neu.edu>
2014-11-08 15:35:20 -0500

    updatin styles for date, info

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var apod = 'https://astronomy-pic-of-the-day.herokuapp.com/api.json';
-var picture, 
+var fallback = new Object();
+fallback.url = 'http://apod.nasa.gov/apod/image/1408/m57_nasagendler_3000.jpg';
+fallback.text = 'Head to http://apod.nasa.gov/ for more great astronomy pictures';
+
+var apod = 'https://astronomy-pic-of-the-day.herokuapp.com/api.json',
+    picture, 
     info;
 
 function parseAPOD(url) {
@@ -8,6 +12,9 @@ function parseAPOD(url) {
     dataType: 'json',
     success: function(data) {
       parseData(data);
+    },
+    error: function() {
+      setData(fallback.url, fallback.text);
     }
   });
 }
@@ -16,16 +23,35 @@ function parseData(data) {
   if (data) {
     picture = data.url;
     info = data.text;
-  } else {
-    picture = fallback.url;
-    info = fallback.text;
   }
   setData(picture, info);
 }
 
 function setData(picture, info) {
+  var now, formattedDate, months;
   $("#background").attr("src", picture);
   $('.info').html(info);
+
+  // set date
+  now = new Date();
+  months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  formattedDate = months[now.getMonth()] + ' ' + now.getDate();
+  $('.date').html(formattedDate); 
 }
 
-parseAPOD(apod);
+function displayInfo() {
+  console.log($(".info-icon"));
+  $('.info-icon').click( function() {
+      $('.info').slideToggle('slow');
+    }
+  );  
+}
+
+function init() {
+  parseAPOD(apod);
+  $( document ).ready(function() {
+    displayInfo();
+  });
+}
+
+init();
